5505b93da5571805207795c7d6324ddf439a0f66
subvod <s.mike234@live.com>
2016-10-15 02:07:38 -0500

    FASM printf()
    Code for use with Flat Assembler. Imports printf() from the MS C Runtime and uses it appropriately.

---
+++ printf.asm
@@ -0,0 +1,22 @@
+format PE console
+entry start
+
+include 'WIN32A.INC'
+
+section '.text' code executable
+start:
+	push hello
+	call [printf]
+	pop ecx
+
+	push 0
+	call [ExitProcess]
+
+section '.rdata' data readable
+	hello db 'Hello world!', 10, 0
+
+section '.idata' data readable import
+	library kernel32, 'kernel32.dll', \
+		msvcrt,   'msvcrt.dll'
+	import kernel32, ExitProcess, 'ExitProcess'
+	import msvcrt, printf, 'printf'
