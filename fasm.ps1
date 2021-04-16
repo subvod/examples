e5fed7aa1038fc91d27256e62ca836f4af83e547
subvod <s.mike234@live.com>
2021-04-16 17:25:52 -0600

    NPPExec for FASM (PowerShell)
    Added fixing INCLUDE paths in source files.

--- fasm.ps1
+++ fasm.ps1
@@ -1,24 +1,45 @@
-# NPPExec Script in PowerShell
+# FASM.PS1
+# PowerShell script for NPPExec
+# Automatically fixes include directories per <source> file
+# Uses default include directory format "fasmw17327\INCLUDE\"
+
 # usage:
 # fasm.ps1 -f <source>
 
-# parameters/arguments
-param (
-  [string]$f = "null"
-)
+$incOrg = "include `""
+$incFix = "include `"INCLUDE"
 
+# folder path of "fasm.exe"
+# REMEMBER TO INCLUDE THE FINAL SLASH
 $fasmDir = "C:\fasmw17327\"
 
+$inFile = $args[0]
+
 # check for null file
-if ($f -eq "null") {
+# exit if true
+if ($inFile -eq "null") {
   Write-Output "ERROR: no file specified"
   exit
 }
 
-# rename output file as <source>.exe
-$outFile = (Get-Item $f).DirectoryName + "\" + (Get-Item $f).Basename + ".exe"
-# $outFile = $outFile + (Get-Item $f).Basename + ".exe"
+# check for matches (improper include directories in <source>)
+if (Select-String -Path $inFile -Pattern $incFix -SimpleMatch -Quiet) {
+  Write-Output "include directories unchanged; continuing..."
+} else {
+  # if include directories need fixing
+  # replace entries found and overwrite <source>
+  Write-Output "fixing include directories..."
+  (Get-Content $inFile).replace($incOrg, $incFix + "\") | Set-Content $inFile
+}
+
+# rename output file as <source>.exe in same path
+Write-Output "constructing command..."
+$outFile = (Get-Item $inFile).DirectoryName + "\" + (Get-Item $inFile).Basename + ".exe"
 # change directory to FASM location
 Set-Location -Path $fasmDir
 # assemble <source> file
-&($fasmDir + "fasm.exe") $f $outFile
+Write-Output "assembling..."
+&($fasmDir + "fasm.exe") $inFile $outFile
+Write-Output "assembly complete:"
+$inFile
+$outFile
